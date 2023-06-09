bc45c92ab0a1b03b94869bfffffa193a55eeb3c9
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-06-09 19:45:30 +0700

    API Update Profile

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -136,4 +136,22 @@ class UserController extends Controller
             );
         }
     }
+
+    public function fetch(Request $request)
+    {
+        return ResponseFormatter::success(
+            $request->user(),
+            'Data profile user berhasil diambil'
+        );
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $data = $request->all();
+
+        $user = Auth::user();
+        $user->update($data);
+
+        return ResponseFormatter::success($user, 'Profile Updated');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [UserController::class, 'fetch']);
+    Route::post('user', [UserController::class, 'updateProfile']);
 });
 
 Route::get('service', [ServiceController::class, 'all']);
