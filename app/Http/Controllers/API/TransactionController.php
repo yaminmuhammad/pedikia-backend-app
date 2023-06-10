dd56092a84aeb43b1f926b4b93ea16616b02ab3b
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-06-10 10:30:21 +0700

    Fix Algo

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -46,23 +46,81 @@ class TransactionController extends Controller
     //         'Data list transaksi berhasil diambil',
     //     );
     // }
-    static function getLeastBusyTherapist()
+    // static function getLeastBusyTherapist()
+    // {
+    //     if (Transaction::count() == 0) {
+    //         return User::where('roles', '=', 'therapist')->first();
+    //     } else {
+    //         $joblessTherapists = DB::table("users")
+    //             ->select()
+    //             ->whereNotIn('users.id', DB::table("transactions")->select('therapist_id'))
+    //             ->where('roles', '=', 'therapist')
+    //             ->get();
+
+    //         if ($joblessTherapists->count() == 0) {
+    //             $leastBusyTherapist = DB::table("users")
+    //                 ->join('transactions', 'users.id', "=", "transactions.therapist_id")
+    //                 ->select('users.id', DB::raw('COUNT(transactions.therapist_id) as jobcount'))
+    //                 ->groupBy('users.id')
+    //                 ->orderBy('jobCount', 'asc')
+    //                 ->first();
+
+    //             return User::where('id', '=', $leastBusyTherapist->id)->first();
+    //         } else {
+    //             return $joblessTherapists->first();
+    //         }
+    //     }
+    // }
+
+    // public function checkout(Request $request)
+    // {
+    //     $request->validate([
+    //         'therapist_id' => 'nullable',
+    //         'total_price' => 'required',
+    //         'extra_price' => 'required',
+    //         'status' => 'required|in:PENDING,SUCCESS,CANCELLED,FAILED'
+    //     ]);
+
+    //     try {
+    //         $user = Auth::user();
+    //         if ($user->roles != "therapist") {
+    //             $therapistId = TransactionController::getLeastBusyTherapist()->id;
+    //             $userId = Auth::user()->id;
+    //             Transaction::create([
+    //                 'users_id' => $userId,
+    //                 'therapist_id' => $therapistId,
+    //                 'address' => $request->address,
+    //                 'total_price' => $request->total_price,
+    //                 'extra_price' => $request->extra_price,
+    //                 'status' => $request->status
+    //             ]);
+    //             $transaction = Transaction::where('users_id', $userId)->first();
+    //             return ResponseFormatter::success(['report' => $transaction], 'Transaction Created');
+    //         } else {
+    //             return ResponseFormatter::error(null, "therapist cannot create transactions.");
+    //         }
+    //     } catch (Exception $err) {
+    //         return ResponseFormatter::error([
+    //             'message' => 'Something went wrong',
+    //             'error' => $err
+    //         ], 'Server Error', 500);
+    //     }
+    // }
+
+    public function getLeastBusyTherapist()
     {
         if (Transaction::count() == 0) {
             return User::where('roles', '=', 'therapist')->first();
         } else {
-            $joblessTherapists = DB::table("users")
-                ->select()
-                ->whereNotIn('users.id', DB::table("transactions")->select('therapist_id'))
+            $joblessTherapists = User::whereNotIn('id', Transaction::select('therapist_id'))
                 ->where('roles', '=', 'therapist')
                 ->get();
 
             if ($joblessTherapists->count() == 0) {
-                $leastBusyTherapist = DB::table("users")
-                    ->join('transactions', 'users.id', "=", "transactions.therapist_id")
+                $leastBusyTherapist = User::join('transactions', 'users.id', '=', 'transactions.therapist_id')
                     ->select('users.id', DB::raw('COUNT(transactions.therapist_id) as jobcount'))
                     ->groupBy('users.id')
-                    ->orderBy('jobCount', 'asc')
+                    ->orderBy('jobcount', 'asc')
                     ->first();
 
                 return User::where('id', '=', $leastBusyTherapist->id)->first();
@@ -84,20 +142,20 @@ class TransactionController extends Controller
         try {
             $user = Auth::user();
             if ($user->roles != "therapist") {
-                $therapistId = TransactionController::getLeastBusyTherapist()->id;
+                $therapistId = $this->getLeastBusyTherapist()->id;
                 $userId = $user->id;
                 Transaction::create([
-                    'users_id' => $userId,
+                    'user_id' => $userId,
                     'therapist_id' => $therapistId,
                     'address' => $request->address,
                     'total_price' => $request->total_price,
                     'extra_price' => $request->extra_price,
                     'status' => $request->status
                 ]);
-                $transaction = Transaction::where('users_id', $userId)->first();
+                $transaction = Transaction::where('user_id', $userId)->first();
                 return ResponseFormatter::success(['report' => $transaction], 'Transaction Created');
             } else {
-                return ResponseFormatter::error(null, "therapist cannot create transactions.");
+                return ResponseFormatter::error(null, "Therapists cannot create transactions.");
             }
         } catch (Exception $err) {
             return ResponseFormatter::error([
